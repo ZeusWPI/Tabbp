ec8b9fa7bd42d4bd9d6815d59ee7df98fb8c6392
redfast00 <redfast00@gmail.com>
2019-05-29 00:29:57 +0200

    Cleaner code

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import json
 import urllib.parse
+import re
 
 from flask import Flask, redirect, url_for, session, jsonify, request
 from flask_oauthlib.client import OAuth, OAuthException
@@ -45,8 +46,7 @@ def tokens():
     # Look, I know this is bad
     # The reason why I did is, is that different databasedrivers (sqlite, mysql, ...)
     # have different ways to prepare SQL statements, and these are incompatible
-    testRegex = re.compile("[a-zA-Z\-_0-9]+")
-    if testRegex.match(session['username']).endpos != len(session['username']):
+    if not re.match(r'[a-zA-Z\-_0-9]+$', session['username']):
         return 'Username should only contain letters, numbers, - or _'
     with tab_engine.connect() as tab_connection, tap_engine.connect() as tap_connection:
         tab_query_result = tab_connection.execute("SELECT `key` FROM users WHERE name = '%s'" % session['username']).first()
